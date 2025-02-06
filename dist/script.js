952604e78331cb0342856c5dc12d5e794b54fe43
OmarAltabbaa <54678565+OmarAltabbaa@users.noreply.github.com>
2025-02-06 17:43:49 +0000

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,7 @@
+
+        document.addEventListener("DOMContentLoaded", function () {
+            const navBar = document.createElement("nav");
+            navBar.innerHTML = '<a href="index.html">Home</a>';
+            document.body.prepend(navBar);
+        });
+        
