647cb709d9c3b8d16799f4c08143faf8660f5bd2
OmarAltabbaa <54678565+OmarAltabbaa@users.noreply.github.com>
2025-02-06 16:43:00 +0000

    Delete vef2-2025-v1-main directory

--- vef2-2025-v1-main/dist/script.js
+++
@@ -1,7 +0,0 @@
-
-        document.addEventListener("DOMContentLoaded", function () {
-            const navBar = document.createElement("nav");
-            navBar.innerHTML = '<a href="index.html">Home</a>';
-            document.body.prepend(navBar);
-        });
-        
